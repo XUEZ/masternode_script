97ce2c238a1f45d428cf54cccaee51802f4b41d7
ABlockNutThermo <38297230+ABlockNutThermo@users.noreply.github.com>
2018-04-11 23:12:30 +0100

    Add files via upload

---
+++ Xuez_Setup.sh
@@ -0,0 +1,72 @@
+#/bin/bash
+cd ~
+echo "****************************************************************************"
+echo "* This script will install and configure your XUEZ Coin masternodes.       *"
+echo "*                    Love from A_Block_Nut(Thermo) ;)                      *"
+echo "*    If you have any issues please ask for help on the XUEZ discord.       *"
+echo "*                      https://discord.gg/QWcK5Yk                          *"
+echo "*                        https://xuezcoin.com/                             *"
+echo "****************************************************************************"
+echo "" 
+echo ""
+echo ""
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!                                                 !"
+echo "! Make sure you double check before hitting enter !"
+echo "!                                                 !"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo ""
+echo ""
+echo ""
+echo "Do you want to install all needed updates (no if you did it before)? [y/n]"
+read DOSETUP
+	if [[ $DOSETUP =~ "y" ]] ; then
+	sudo apt-get update
+	sudo apt-get -y upgrade
+	sudo apt-get -y dist-upgrade
+cd /var
+sudo touch swap.img
+sudo chmod 600 swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+sudo mkswap /var/swap.img
+sudo swapon /var/swap.img
+sudo free
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+cd
+sudo apt-get install -y ufw
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw allow 41798/tcp
+sudo ufw logging on
+echo "y" | sudo ufw enable
+sudo ufw status
+fi
+wget https://github.com/XUEZ/xuez/releases/download/1.0.1.7/xuez-linux-cli-1017.tgz
+tar -xvzf xuez-linux-cli-1017.tgz
+echo ""
+echo "Configure your masternodes now!"
+echo "Type the IP of this server, followed by [ENTER]:"
+read IP
+echo ""
+echo "Enter masternode private key for node $ALIAS"
+read PRIVKEY
+CONF_DIR=~/.xuez\/
+CONF_FILE=xuez.conf
+PORT=41798
+mkdir -p $CONF_DIR
+echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
+echo "rpcpassword=passw"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
+echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
+echo "listen=1" >> $CONF_DIR/$CONF_FILE
+echo "server=1" >> $CONF_DIR/$CONF_FILE
+echo "daemon=1" >> $CONF_DIR/$CONF_FILE
+echo "logtimestamps=1" >> $CONF_DIR/$CONF_FILE
+echo "maxconnections=256" >> $CONF_DIR/$CONF_FILE
+echo "masternode=1" >> $CONF_DIR/$CONF_FILE
+echo "" >> $CONF_DIR/$CONF_FILE
+echo "" >> $CONF_DIR/$CONF_FILE
+echo "port=$PORT" >> $CONF_DIR/$CONF_FILE
+echo "masternodeaddr=$IP:$PORT" >> $CONF_DIR/$CONF_FILE
+echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/$CONF_FILE
+./xuez-cli -daemon
+echo "if server start failure try ./xuezd -reindex"
