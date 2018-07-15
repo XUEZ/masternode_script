f0737522582372abfa534130d33f040fef4a436a
ABlockNutThermo <38297230+ABlockNutThermo@users.noreply.github.com>
2018-07-16 00:19:39 +0100

    Update UpdateXUEZ.sh

--- UpdateXUEZ.sh
+++ UpdateXUEZ.sh
@@ -3,18 +3,14 @@ cd ~
 echo "****************************************************************************"
 echo "*                        XUEZ WALLET UPDATE SCRIPT                         *"
 echo "****************************************************************************"
-./xuez-cli stop
-pkill -9 xuezd
-rm ~/xuez-cli
-rm ~/xuezd
-rm ~/xuez-linux-cli.tgz
-rm ~/xuez-tx
-echo ""
+./xuez-cli stop 
+rm xuezd 
+rm xuez-cli 
+rm xuez-tx 
 wget https://github.com/XUEZ/xuez/releases/download/1.0.1.10/xuez-linux-cli-10110.tgz 
-echo ""
-echo ""
-tar -xvzf xuez-linux-cli-10110.tgz
-./xuezd -daemon -reindex
+tar -xf xuez-linux-cli-10110.tgz 
+rm xuez-linux-cli-10110.tgz 
+./xuezd -daemon -reindex 
 echo "****************************************************************************"
 echo "*                        UPDATE COMPLETE                                   *"
 echo "****************************************************************************"
