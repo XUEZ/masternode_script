05b94122a13f479056bbba565cafb6a707dffb7c
God-Father <33990385+johnlito123@users.noreply.github.com>
2018-07-20 02:00:55 +0100

    Update XuezUpdate.sh

--- XuezUpdate.sh
+++ XuezUpdate.sh
@@ -44,27 +44,24 @@ echo "Are you running this script as the root user? [y/n], followed by [ENTER]"
 echo ""
 echo "***********************************************************************************"
 echo "* It is very important to program your masternode under a user rather than root.	*"
-echo "* By entering Yes you will create a new user.										*"
+echo "* By entering Yes you will create a new user.					*"
 echo "* You Dont need to enter any personal details but you do need to create password.	*"
-echo "*																					*"
-echo "* OPTIONS BELOW																	*"
-echo "*																					*"
-echo "* Please enter Yes If you are running this Script as root then re-run the script	*"
-echo "* Please enter Yes If you want to get into your created user						*"
-echo "*																					*"
-echo "* Please Enter No if you are running this Script under your new user.				*"
+echo "*											*"
+echo "* OPTIONS BELOW									*"
+echo "*											*"
+echo "* 1 - Enter Yes If you are running this Script as root then re-run the script	*"
+echo "* 2 - Enter Yes If you want to get into your created user			*"
+echo "*											*"
+echo "* 3 - Please Enter No if you are running this Script under your new user.		*"
 echo "***********************************************************************************"
 
 read USETUP
 	if 
 	[[ $USETUP =~ "y" ]] || [[$USETUP =~ "Y" ]] ; then
 	
-sudo adduser -s /bin/echo ""
-echo ""bash -d /home/xuez xuez
-#user will enter a password for the xuez account/user
-passwd xuez
+sudo adduser xuez
 usermod -aG sudo xuez
-ssh-keygen -t rsa -b 4096 -P "" -f "/home/xuez/.ssh/id_rsa"
++su - xuez
 fi
 echo ""
 echo ""
